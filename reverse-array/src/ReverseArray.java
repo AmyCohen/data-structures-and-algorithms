bcc5ca8d8dce5cfe942437159cc61a16c5e18dc1
Amy Cohen <anbcohen@gmail.com>
2018-07-10 06:48:38 -0700

    cleaned up codebase by removing zombie code

--- reverse-array/src/ReverseArray.java
+++ reverse-array/src/ReverseArray.java
@@ -17,27 +17,3 @@ public class ReverseArray {
         return reversedNumbers;
     }
 }
-
-/*
-public class ReverseArray {
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6};
-        reverseArray(numbers);
-    }
-
-    public static void reverseArray(int[] numbers) {
-        int[] reversedNumbers = new int[numbers.length];
-        int index = 0;
-
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            reversedNumbers[index++] = numbers[i];
-        }
-        numbers = reversedNumbers;
-    }
-}
- */
-
-/*
-learned about reversing arrays from this site:
-http://www.speakingcs.com/2015/07/how-to-reverse-array-in-java-with-and.html
- */
--- reverse-array/src/ReverseArrayTest.java
+++ reverse-array/src/ReverseArrayTest.java
@@ -16,22 +16,3 @@ class ReverseArrayTest {
 
     }
 }
-
-
-/*
-FROM EXAMPLE:
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
-class D01_ArrayReverseTest {
-    @Test
-    public void myTest() {
-        int[] numbers = {1, 2, 3};
-        D01_ArrayReverse.doubler(numbers);
-        int[] expected = {2, 4, 6};
-        assertArrayEquals(numbers, expected);
-    }
-}
-*** PASSED ***
- */
