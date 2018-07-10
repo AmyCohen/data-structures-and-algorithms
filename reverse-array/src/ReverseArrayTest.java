12ad368a7f305698e9e218a36523aae7787d7951
Amy Cohen <anbcohen@gmail.com>
2018-07-10 09:35:45 -0700

    refactored the code to allow for arguments when the function is called and tested.

--- reverse-array/src/ReverseArrayTest.java
+++ reverse-array/src/ReverseArrayTest.java
@@ -6,7 +6,11 @@ class ReverseArrayTest {
     @Test
 
     public void reverseArrayTest() {
-        int[] numbers = ReverseArray.reverseArray();
+        //        int[] originalNumbers = {1, 2, 3, 4, 5, 6};
+//                int[] originalNumbers = {89, 2354, 3546, 23, 10, -923, 823, -12};
+        int[] originalNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+
+        int[] numbers = ReverseArray.reverseArray(originalNumbers);
 
 //        int[] expected = {6, 5, 4, 3, 2, 1};
 //        int[] expected = {-12, 823, -923, 10, 23, 3546, 2354, 89};
