cd2cb2a24c6b176d57a03bfda54bc8ac9b0c1a1d
Amy Cohen <anbcohen@gmail.com>
2018-07-10 21:49:19 -0700

    wrote several test cased and all have passed

--- src/day02/ArrayShift.java
+++ src/day02/ArrayShift.java
@@ -5,23 +5,24 @@ import java.util.Arrays;
 
 public class ArrayShift {
     public static void main(String[] args) {
-        int[] practiceArray = {2, 4, 6, 8};
-        int[] practiceAdd = {5};
+//        int[] practiceArray = {2, 4, 6, 8};
+//        int[] practiceAdd = {5};
 
 //        int[] practiceArray = {4,8,15,23,42};
 //        int[] practiceAdd = {16};
+
+        int[] practiceArray = {1, 2, 4,8,15,23,42, 100, 33};
+        int[] practiceAdd = {18};
 
 
         insertShiftArray(practiceArray, practiceAdd);
     }
 
-    public static String insertShiftArray(int[] numbersArray, int[] addNumber) {
+    public static int[] insertShiftArray(int[] numbersArray, int[] addNumber) {
         int[] newNumbersArray = new int[numbersArray.length + 1];
         double findingHalf = (double)numbersArray.length/2;
-        System.out.println(findingHalf);
 
         double halfOfArray = Math.ceil(findingHalf);
-        System.out.println(halfOfArray);
 
         for (int i = 0; i < newNumbersArray.length; i++) {
             if (i < halfOfArray) {
@@ -33,6 +34,12 @@ public class ArrayShift {
             }
         }
         System.out.println(Arrays.toString(newNumbersArray));
-        return Arrays.toString(newNumbersArray);
+        return newNumbersArray;
     }
 }
+
+
+
+
+
+
--- src/day02/ArrayShiftTest.java
+++ src/day02/ArrayShiftTest.java
@@ -1,13 +1,53 @@
-//package day02;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class ArrayShiftTest {
-//
-//    @Test
-//    void insertShiftArray() {
-//        AssertEquals(true, true);
-//    }
-//}
+package day02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayShiftTest {
+
+    @Test
+    void shortExampleProvided() {
+        int[] practiceArray = {2, 4, 6, 8};
+        int[] practiceAdd = {5};
+        int[] actual = ArrayShift.insertShiftArray(practiceArray, practiceAdd);
+
+        int[] expected = {2,4,5,6,8};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mediumExampleProvided() {
+        int[] practiceArray = {4,8,15,23,42};
+        int[] practiceAdd = {16};
+        int[] actual = ArrayShift.insertShiftArray(practiceArray, practiceAdd);
+
+        int[] expected = {4,8,15,16,23,42};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void longOriginalTestArray() {
+        int[] practiceArray = {1, 2, 4,8,15,23,42, 100, 33};
+        int[] practiceAdd = {18};
+        int[] actual = ArrayShift.insertShiftArray(practiceArray, practiceAdd);
+
+        int[] expected = {1, 2, 4,8,15, 18, 23,42, 100, 33};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void emptyArrayTestWithValue() {
+        int[] practiceArray = {};
+        int[] practiceAdd = {18};
+        int[] actual = ArrayShift.insertShiftArray(practiceArray, practiceAdd);
+
+        int[] expected = {18};
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
