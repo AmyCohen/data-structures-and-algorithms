d7cbd18a5dacfd2fc36c337d593d3468a9427f69
Amy Cohen <anbcohen@gmail.com>
2018-07-19 21:36:31 -0700

    had trouble with git on the train but just added the pictures

---
+++ src/day09/day09LinkedListTest.java
@@ -0,0 +1,58 @@
+package day09;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class day09LinkedListTest {
+
+    @Test
+    void LoopedList() {
+
+        day09LinkedList loopy = new day09LinkedList();
+
+        day09ListNode n1 = new day09ListNode(1);
+        day09ListNode n2 = new day09ListNode(7);
+        day09ListNode n3 = new day09ListNode(2);
+        day09ListNode n4 = new day09ListNode(3);
+        day09ListNode n5 = new day09ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n2;
+
+        loopy.root = n1;
+
+        Boolean actual = loopy.hasLoop();
+        Boolean expected = true;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void NotLoopedList() {
+
+        day09LinkedList loopy = new day09LinkedList();
+
+        day09ListNode n1 = new day09ListNode(1);
+        day09ListNode n2 = new day09ListNode(7);
+        day09ListNode n3 = new day09ListNode(2);
+        day09ListNode n4 = new day09ListNode(3);
+        day09ListNode n5 = new day09ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        loopy.root = n1;
+
+        Boolean actual = loopy.hasLoop();
+        Boolean expected = false;
+
+        assertEquals(expected, actual);
+    }
+
+}
---
+++ src/day09/day09ListNode.java
@@ -0,0 +1,12 @@
+package day09;
+
+public class day09ListNode {
+    public int data;
+    public day09LinkedList value;
+    public day09ListNode next;
+
+    public day09ListNode(int value){
+        this.data=value;
+        this.next=null;
+    }
+}
