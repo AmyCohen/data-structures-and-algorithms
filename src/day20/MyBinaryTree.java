ec87299d3c4f4afa5e2e0c8de6a9c9c6a43ae6d4
Amy Cohen <anbcohen@gmail.com>
2018-08-04 19:27:36 -0700

    got help from stackoverflow to understand why I kept missing the bottom nodes or getting null pointer exceptions. this now passes all tests

--- src/day20/MyBinaryTree.java
+++ src/day20/MyBinaryTree.java
@@ -4,46 +4,30 @@ package day20;
 public class MyBinaryTree {
     public TreeNode root;
 
-    public TreeNode flip() {
-        return flipHelper(this.root);
+    public void flip() {
+        flipHelper(this.root);
     }
 
     private static TreeNode flipHelper(TreeNode current) {
-
-        //if current and the left/right are null, the tree is empty
-        if (current == null && current.left == null && current.right == null) {
-            return null;
-        }
-
-        //separate from above since a node could be null that the recursion is looking at without the rest of the tree being null
-        if (current == null || (current.left == null && current.right == null)) {
-            return null;
-        }
-
-        if (current.left == null){
-            current.left = current.right;
-            current.right = null;
-        } else if (current.right == null) {
-            current.right = current.left;
-            current.left = null;
-        }
-
-        if (current.left !=null && current.right !=null) {
+        //hold the value of the right node before changing it so it can be applied correctly to the left at the end
         TreeNode newLeft = current.right;
-            //hold on to a reference to the right node
+        current.right = current.left;
+        current.left = newLeft;
 
-            //change the right node to be what the left node is
-            current.right = current.left;
-
-            //use the value stored to replace the value on the left
-            current.left = newLeft;
+        //I was trying to combine these two "ifs" on my original logic and that is what was messing me up.
+        if (current.left != null) {
+            flipHelper(current.left);
         }
 
-        flipHelper(current.left);
-        flipHelper(current.right);
-
-
+        if (current.right != null) {
+            flipHelper(current.right);
+        }
 
         return current;
     }
 }
+
+/*
+    I don't understand why my original logic didn't work but I'll keep studying it.  I followed this example for how to solve it.
+    https://stackoverflow.com/questions/9460255/reverse-a-binary-tree-left-to-right
+ */
--- src/day20/MyBinaryTreeTest.java
+++ src/day20/MyBinaryTreeTest.java
@@ -26,11 +26,50 @@ class MyBinaryTreeTest {
         boolean leftEquals6 = lycra.root.left.data == 6;
         boolean rightEquals2 = lycra.root.right.data == 2;
         boolean leftEqualsNull = lycra.root.left.left == null;
-//        boolean bottomNodeEquals1 = lycra.root.left.right.left.data == 1;
+        boolean bottomNodeEquals1 = lycra.root.left.right.left.data == 1;
 
         assertTrue(leftEquals6);
         assertTrue(rightEquals2);
         assertTrue(leftEqualsNull);
-//        assertTrue(bottomNodeEquals1);
+        assertTrue(bottomNodeEquals1);
+    }
+
+    @Test
+    void flippedAgain() {
+        MyBinaryTree lycra = new MyBinaryTree();
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n5 = new TreeNode(5);
+
+        lycra.root = n8;
+        n8.left= n2;
+        n8.right = n6;
+        n6.left = n7;
+        n7.right = n1;
+        n7.left = n5;
+
+        lycra.flip();
+
+        boolean leftEquals6 = lycra.root.left.data == 6;
+        boolean rightEquals2 = lycra.root.right.data == 2;
+        boolean leftEqualsNull = lycra.root.left.left == null;
+        boolean bottomLeftEquals1 = lycra.root.left.right.left.data == 1;
+        boolean bottomRightEquals5 = lycra.root.left.right.right.data == 5;
+
+        assertTrue(leftEquals6);
+        assertTrue(rightEquals2);
+        assertTrue(leftEqualsNull);
+        assertTrue(bottomLeftEquals1);
+        assertTrue(bottomRightEquals5);
+    }
+
+    @Test
+    void flippedNothing() {
+        MyBinaryTree lycra = new MyBinaryTree();
+        boolean equalsNull = lycra.root == null;
+        assertTrue(equalsNull);
     }
 }
