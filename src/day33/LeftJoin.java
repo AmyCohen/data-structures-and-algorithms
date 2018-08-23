fcd2be0267b5e659223c32daa25712c6b3d24392
Amy Cohen <anbcohen@gmail.com>
2018-08-22 19:16:53 -0700

    i'm getting closer, but my foreach loop is not iterating over all the keys in the map

--- src/day33/LeftJoin.java
+++ src/day33/LeftJoin.java
@@ -1,16 +1,26 @@
 package day33;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class LeftJoin {
 
     public static String leftJoin (Map<String, String> map1, Map<String, String> map2){
 
+        String[] row = new String[3];
+        String[][] rowSet = new String[1][row.length];
         MyMap<String, String, String> joinedMap = new MyMap<>();
+        System.out.println("keyset = " + map1.keySet());
         for (String key : map1.keySet()){
 
             if (map2.containsKey(key)){
-                joinedMap.put(key, map1.values().toString(), map2.values().toString());
+                for (int i = 0; i < map1.size()-1; i++) {
+                    row[0] = key;
+                    row[1] = map1.get(key);
+                    row[2] = map2.get(key);
+                    rowSet[i] = row;
+                    System.out.println(Arrays.deepToString(rowSet));
+                }
             }
         }
 
