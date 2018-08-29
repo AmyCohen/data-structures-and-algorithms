b8da44ea6e68f889e1d7733d865c829276dbc124
Amy Cohen <anbcohen@gmail.com>
2018-08-29 12:10:09 -0700

    adding tests to find out where I am wrong in my code. I am getting one side duplicated in a weird way and it is ignoreing the other side. Not sure where that is coming from right now

--- src/day36/MergeSort.java
+++ src/day36/MergeSort.java
@@ -7,18 +7,7 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
 
-//        ArrayList<Integer> unsorted = new ArrayList<>();
-//        unsorted.add(34);
-//        unsorted.add(19);
-//        unsorted.add(42);
-//        unsorted.add(-9);
-//        unsorted.add(2018);
-//        unsorted.add(0);
-//        unsorted.add(2005);
-//        unsorted.add(77);
-//        unsorted.add(2099);
-
-        System.out.println(Arrays.toString(unsorted));
+//        System.out.println(Arrays.toString(unsorted));
 
         mergeSort(unsorted);
     }
@@ -32,52 +21,33 @@ Steve's solution from lecture.
  */
     public static int[] mergeSort(int[] unsorted) {
 
-//        int size = unsorted.length;
-//        int[] result = new int[size];
-//        int half = (int) Math.floor(size / 2);
-
         //If the array is less than 2 (meaning 1 or 0, then it is by default already sorted and an just be returned.
         if (unsorted.length < 2) {
             return unsorted;
         }
 
-        //put the left side of the original array here using the size divided by 2 to create the size of the new leftSide array.
-//        int[] leftSide = new int[half];
-
-        //while the leftSide array will always be the floored version of half, the rightSide array has to account for the fact that the original array may not have been even. So it rounds up to the number that is the original size divided by 2.
-//        int[] rightSide = new int[(int) Math.ceil((double) size / 2)];
-
-//        int[] leftSide = leftSide(unsorted);
-//        int[] rightSide = rightSide(unsorted);
-
         int[] leftSide = leftSide(unsorted);
         int[] rightSide = rightSide(unsorted);
-
-//        for (int i = 0; i < half; i++) {
-//            //populate the left side array by counting up from index 0
-//            leftSide[i] = unsorted[i];
-//            //populate the right side of the array by counting up from the halfway point from the original array
-//            rightSide[i] = unsorted[half + i];
-//        }
-
-        //the above would only work on an even original array. This if takes into consideration that the original array may have been odd and then adds the last number into the right side array. This also avoid null pointer exceptions from trying to do this above.
-//        if (size % 2 == 1) {
-//            rightSide[half] = unsorted[size - 1];
-//        }
 
         //recursively call the function on each side to continue to split the arrays into smaller portions.
         leftSide = mergeSort(leftSide);
         rightSide = mergeSort(rightSide);
 
-        //set the return value to be the answer from the helperfuntion called "stitch"
+        //set the return value to be the answer from the helperfuntion called merge
         int[] result = merge(leftSide, rightSide);
         System.out.println("Result = " + Arrays.toString(result));
         return result;
     }
 
-    private static int[] rightSide(int[] unsorted) {
+    public static int[] rightSide(int[] unsorted) {
+        int half;
         int size = unsorted.length;
-        int half = (int) Math.ceil((double) size / 2);
+
+        if (unsorted.length % 2 == 0) {
+            half = unsorted.length/2;
+        } else {
+            half = (int) Math.ceil((double) size / 2);
+        }
 
         int[] right = new int[half];
 
@@ -89,8 +59,14 @@ Steve's solution from lecture.
 
     }
 
-    private static int[] leftSide(int[] unsorted) {
-        int half = (int)Math.floor(unsorted.length/2);
+    public static int[] leftSide(int[] unsorted) {
+        int half;
+
+        if (unsorted.length % 2 == 0) {
+            half = unsorted.length/2;
+        } else {
+            half = (int) Math.floor(unsorted.length / 2);
+        }
 
         int[] left = new int[half];
 
@@ -132,15 +108,6 @@ Steve's solution from lecture.
             leftIndex++;
         }
 
-//        for (int i = 0; i < size; i++) {
-//            if ((rightIndex == right.length) || (leftIndex != left.length && left[leftIndex] < right[rightIndex])) {
-//                result[i] = left[leftIndex];
-//                leftIndex++;
-//            } else {
-//                result[i] = right[rightIndex];
-//                rightIndex++;
-//            }
-//        }
         return result;
     }
 }
---
+++ src/day36/MergeSortTest.java
@@ -0,0 +1,34 @@
+package day36;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortTest {
+
+    @BeforeEach
+    void setUp() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
+    }
+
+    @Test
+    void mergeSortNormalTest() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
+        int[] sorted = new int[]{-9, 0, 19, 34, 42, 77, 2005, 2018, 2099};
+        String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
+        String expected = Arrays.toString(sorted);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rightSide() {
+    }
+
+    @Test
+    void leftSide() {
+    }
+}
