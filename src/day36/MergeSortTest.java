3588540d5c87b32cbb0870bd9682e77dc91f9f07
Amy Cohen <anbcohen@gmail.com>
2018-08-29 18:50:15 -0700

    added multiple tests and all are passing

--- src/day36/MergeSortTest.java
+++ src/day36/MergeSortTest.java
@@ -9,13 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortTest {
 
-    @BeforeEach
-    void setUp() {
-        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
-    }
-
     @Test
-    void mergeSortNormalTest() {
+    void EmptyArray() {
+        int[] unsorted = new int[]{};
+        int[] sorted = new int[]{};
+        String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
+        String expected = Arrays.toString(sorted);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void mergeSortOddNumArray() {
         int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
         int[] sorted = new int[]{-9, 0, 19, 34, 42, 77, 2005, 2018, 2099};
         String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
@@ -23,12 +27,79 @@ class MergeSortTest {
 
         assertEquals(expected, actual);
     }
-//
-//    @Test
-//    void rightSide() {
-//    }
-//
-//    @Test
-//    void leftSide() {
-//    }
+
+    @Test
+    void mergeSortEvenNumArray() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 2005, 77, 2099};
+        int[] sorted = new int[]{-9, 19, 34, 42, 77, 2005, 2018, 2099};
+        String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
+        String expected = Arrays.toString(sorted);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void mergeSortSmallEvenNumArray() {
+        int[] unsorted = new int[]{34, 19};
+        int[] sorted = new int[]{19, 34};
+        String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
+        String expected = Arrays.toString(sorted);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void mergeSortSmallOddNumArray() {
+        int[] unsorted = new int[]{34, 19, -99};
+        int[] sorted = new int[]{-99, 19, 34};
+        String actual = Arrays.toString(MergeSort.mergeSort(unsorted));
+        String expected = Arrays.toString(sorted);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rightSideOdd5() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
+        String actual = Arrays.toString(MergeSort.rightSide(unsorted));
+        String expected = "[2018, 0, 2005, 77, 2099]";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rightSideEven4() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 2005, 77, 2099};
+        String actual = Arrays.toString(MergeSort.rightSide(unsorted));
+        String expected = "[2018, 2005, 77, 2099]";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rightSideOdd3() {
+        int[] unsorted = new int[]{34, 19, 42};
+        String actual = Arrays.toString(MergeSort.rightSide(unsorted));
+        String expected = "[19, 42]";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void leftSideAlwaysEven4() {
+        int[] unsorted = new int[]{34, 19, 42, -9, 2018, 0, 2005, 77, 2099};
+        String actual = Arrays.toString(MergeSort.leftSide(unsorted));
+        String expected = "[34, 19, 42, -9]";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rightSideSmallEven4() {
+        int[] unsorted = new int[]{34, 19, 42, 99};
+        String actual = Arrays.toString(MergeSort.leftSide(unsorted));
+        String expected = "[34, 19]";
+
+        assertEquals(expected, actual);
+    }
 }
